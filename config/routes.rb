79ffced756362852e587e5081dc9960d29af0dcc
Michaelbennett1989 <bennettmichael1989@gmail.com>
2017-02-08 22:37:10 -0500

    3.8 Index-About-Contact Page Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_page/index'
+  get 'static_pages/about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html.
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+  
+  root 'static_pages#index'
 end
