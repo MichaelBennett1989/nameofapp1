c8ccb4698dadca5d0dee185fab9ed577335f55b1
Michaelbennett1989 <bennettmichael1989@gmail.com>
2017-03-08 13:29:12 -0500

    5.3(3) fixed email address in static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,7 +12,7 @@ class StaticPagesController < ApplicationController
 	  @email = params[:email]
 	  @message = params[:message]
 	  ActionMailer::Base.mail(:from => @email,
-      :to => 'michaelbennet1989@yahoo.com',
+      :to => 'michaelbennett1989@yahoo.com',
       :subject => "A new contact form message from #{@name}",
       :body => @message).deliver_now
   end
