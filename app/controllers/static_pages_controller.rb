c73621930362797e7ccc431d69e281be383c3beb
Michaelbennett1989 <bennettmichael1989@gmail.com>
2017-03-08 13:11:44 -0500

    5.3(1) created sharegrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,15 @@ class StaticPagesController < ApplicationController
   def landing_pages
     @products = Product.limit(3)
   end
+
+
+  def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+      :to => 'michaelbennet1989@yahoo.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
 end
